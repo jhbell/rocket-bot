c0649b0b5a28679c97073f5d46be70160fb770c2
Jeff Bell <jeff@rev.com>
2020-02-24 21:45:40 -0600

    First shot at webhook

--- rocket-bot/__main__.py
+++ rocket-bot/__main__.py
@@ -2,11 +2,36 @@ import os
 
 from aiohttp import web
 
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+router = routing.Router()
 routes = web.RouteTableDef()
 
-@routes.get("/")
+@router.register("pull_request", action="opened")
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """ Whenever an issue is opened, greet the author and say thanks."""
+    issue_url = event.data["pull_request"]["issue_url"]
+    author = event.data["issue"]["user"]["login"]
+    url = f"@{issue_url}/reactions"
+
+    await gh.post(url, data={"content": "rocket"},
+            accept="application/vnd.github.squirrel-girl-preview")
+
+
+@routes.post("/")
 async def main(request):
-    return web.Response(status=200, text="Hello world!")
+    body = await request.read()
+
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+
+    async with aiohttp.ClientSession() as session:
+        gh = ghaiohttp.GitHubApi(session, "jhbell", oauth_token=oauth_token)
+        await router.dispatch(event, gh)
+    return web.Response(status=200)
 
 if __name__ == "__main__":
     app = web.Application()
